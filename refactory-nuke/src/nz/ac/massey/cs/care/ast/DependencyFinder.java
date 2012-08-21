d1728de908650579e029327735f37de6c694da0a
alishah.iiui <alishah.iiui@gmail.com>
2012-08-21 10:49:12 +0000

    null check added

--- refactory-nuke/src/nz/ac/massey/cs/care/ast/DependencyFinder.java
+++ refactory-nuke/src/nz/ac/massey/cs/care/ast/DependencyFinder.java
@@ -105,6 +105,7 @@ public class DependencyFinder extends ASTVisitor{
 	@Override
 	public boolean visit(QualifiedName node){
 		IBinding b = node.resolveBinding();
+		if(targetClass == null) return super.visit(node);
 		if(b!=null && b.getKind() == IBinding.VARIABLE){
 			String qualifier = node.getQualifier().toString();
 			if(qualifier.contains(".")) qualifier = qualifier.substring(qualifier.lastIndexOf(".")+1);
